264c855ee3d77354879b522e689cec16c49968a1
Junioresc <junioresc1092@gmail.com>
2020-10-16 08:44:52 -0400

    conditionally rendered the images and the document title changes depending on the tab you clicked

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,31 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-    const categories = [
-        {
-            name: "Commercial",
-            description: "Photos of grocery stores, food trucks, and other commercial projects"
-        },
-        {
-            name: "Portraits",
-            description: "Portraits of people in my life"
-        },
-        {
-            name: "Food",
-            description: "Delicious delicacies"
-        },
-        {
-            name: "Landscape",
-            description: "Fields, farmhouses, waterfalls, and the beauty of nature"
-        }
-    ];
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props;
 
-    function categorySelected() {
-        console.log("hello")
-    }
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
     return (
-      <header>
+      <header className="flex-row px-1">
         <h2>
           <a data-testid="link" href="/">
             <span role="img" aria-label="camera">
@@ -44,8 +32,8 @@ function Nav() {
                     <span>Contact</span>
                 </li>
                     {categories.map((category) => (
-                        <li className="mx-1" key={category.name}>
-                            <span onClick={() => categorySelected(category.name)}>{category.name}</span>
+                        <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
+                            <span onClick={() => setCurrentCategory(category)}>{capitalizeFirstLetter(category.name)}</span>
                         </li>
                     ))}
             </ul>
