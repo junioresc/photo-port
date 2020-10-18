1705b0ab9e5b34aa82780b74d3241d2fc80e0694
Junioresc <junioresc1092@gmail.com>
2020-10-17 23:39:07 -0400

    conditionally rendered the contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,36 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
     const { name, email, message } = formState;
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
+        if(e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if(!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if(!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
     }
     
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(formState);
+        if (!errorMessage) {
+          setFormState({ ...formState, [e.target.name]: e.target.value });
+          console.log(formState);
+        }
     }
+
+
 
     return (
       <section>
@@ -22,7 +42,7 @@ function ContactForm() {
               type="text"
               name="name"
               defaultValue={name}
-              onChange={handleChange}
+              onBlur={handleChange}
             />
           </div>
           <div>
@@ -31,7 +51,7 @@ function ContactForm() {
               type="email"
               name="email"
               defaultValue={email}
-              onChange={handleChange}
+              onBlur={handleChange}
             />
           </div>
           <div>
@@ -40,9 +60,14 @@ function ContactForm() {
               name="message"
               rows="5"
               defaultValue={message}
-              onChange={handleChange}
+              onBlur={handleChange}
             ></textarea>
           </div>
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
           <button type="submit">Submit</button>
         </form>
       </section>
